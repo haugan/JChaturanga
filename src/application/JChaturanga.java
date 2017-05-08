8266bd5becaa46d26f1b2f2cd5425e08c5ed10fb
Marius Haugan <hauganmarius@gmail.com>
2017-05-08 10:59:21 +0200

    Drawing squares
    Wrote code to draw square graphics in a 8*8 grid on the main game scene.
    Each square is a stack pane which adds an extended rectangle class
    "square" for the background graphics with an added canvas "piece
    graphics" class for the game pieces. But, the piece graphics aren't
    implemented yet.

--- src/application/JChaturanga.java
+++ src/application/JChaturanga.java
@@ -8,14 +8,14 @@ import javafx.stage.Stage;
 public class JChaturanga extends Application {
 
     public static void main(String[] args) {
-        Board b = Board.initializeBoard();
-        System.out.println(b); // print ASCII board
+        Board boardEngine = Board.initializeBoard();
+        System.out.println(boardEngine); // print ASCII board
         launch(args); // launch JavaFX
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("ASD3000: application.JChaturanga");
+        primaryStage.setTitle("ASD3000: JChaturanga 0.5");
         primaryStage.setScene(new MainGUI().getGameScene());
         primaryStage.show();
     }
--- src/gui/board/ChessBoardGrid.java
+++ src/gui/board/ChessBoardGrid.java
@@ -3,6 +3,8 @@ package gui.board;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,29 +14,64 @@ import static engine.board.BoardUtilities.SQUARES_ON_ROW;
 
 public class ChessBoardGrid extends GridPane {
 
+    public static final int BOARD_WIDTH = 600;
+    public static final int BOARD_HEIGHT = 600;
+    public static final int SQUARE_WIDTH = BOARD_WIDTH / 8;
+    public static final int SQUARE_HEIGHT = BOARD_HEIGHT / 8;
+    public static final Color LIGHT_SQUARE = Color.ALICEBLUE;
+    public static final Color DARK_SQUARE = Color.CADETBLUE;
+
     public ChessBoardGrid() {
-        setPrefSize(600,600);
-        final List<SquarePane> boardSquares = new ArrayList<>(64);
-        int pos = 0;
+        setPrefSize(BOARD_WIDTH, BOARD_HEIGHT);
+        final List<SquareStack> boardSquares = new ArrayList<>(64);
+        SquareStack boardSquare;
         for (int col = 0; col < SQUARES_ON_COL; col++) {
             for (int row = 0; row < SQUARES_ON_ROW; row++) {
-                pos++;
-                SquarePane boardSquare = new SquarePane();
-                boardSquares.add(boardSquare); // add square pane to list of panes
-                this.add(boardSquare, col, row);
-                System.out.println("square: " + col + "," + row + " = " + (pos));
+                if ((col + row) % 2 == 0) {
+                    boardSquare = new SquareStack(0,0, LIGHT_SQUARE);
+                } else {
+                    boardSquare = new SquareStack(0,0, DARK_SQUARE);
+                }
+                boardSquares.add(boardSquare); // add square stack to list of panes
+                this.add(boardSquare, col, row); // add square stack to parent grid
+                System.out.println("square: " + col + "," + row);
             }
         }
     }
 
     // INNER CLASS!
-    public class SquarePane extends StackPane {
-        // TODO: implement by adding piece graphics to stack?
+    public class SquareStack extends StackPane {
+
+        double xPos, yPos;
+        Color backgroundColor;
+        SquareGraphic squareGraphic;
+        PieceGraphic pieceGraphic;
+
+        public SquareStack(double xPos, double yPos, Color backgroundColor) {
+            this.xPos = xPos;
+            this.yPos = yPos;
+            this.backgroundColor = backgroundColor;
+            squareGraphic = new SquareGraphic(xPos, yPos, backgroundColor);
+            pieceGraphic = new PieceGraphic();
+            this.getChildren().addAll(squareGraphic, pieceGraphic);
+        }
+
     }
 
     // INNER CLASS!
-    public class PieceGraphics extends Canvas {
-        // TODO: implement movable piece graphics with canvas, or more suitable class?
+    public class SquareGraphic extends Rectangle {
+
+        public SquareGraphic(double xPos, double yPos, Color backgroundColor) {
+            super(SQUARE_WIDTH, SQUARE_HEIGHT, backgroundColor);
+            this.setX(xPos); // position is relative to parent node (i.e. StackPane)
+            this.setY(yPos); // position is relative to parent node (i.e. StackPane)
+        }
+
+    }
+
+    // INNER CLASS!
+    public class PieceGraphic extends Canvas {
+        // TODO: nodes can be styled with CSS!
     }
 
 }
